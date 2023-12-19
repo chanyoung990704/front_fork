0f9a642a8e7835b16b6032069f725b8a225984dc
CYPARK <chanyoung990704@naver.com>
2023-12-20 01:36:30 +0900

    update movieSearch Function

--- src/components/movie-grid/MovieGrid.jsx
+++ src/components/movie-grid/MovieGrid.jsx
@@ -1,184 +1,163 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-
 import './movie-grid.scss';
-
 import MovieCard from '../movie-card/MovieCard';
-import Button, { OutlineButton } from '../button/Button';
-import Input from '../input/Input'
-
+import { OutlineButton } from '../button/Button';
 import tmdbApi, { category, movieType, tvType } from '../../api/tmdbApi';
 import { movieSearch } from '../../api/PostApiService';
 
+function useClickOutside(handler) {
+  const domNodeRef = useRef();
 
-
-function AutoComplete() {
-    const [inputValue, setInputValue] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
-    const navigate = useNavigate();
-  
-    const autoComplete = async () => {
-      if (inputValue.length >= 3) {
-        try {
-          const response = await movieSearch(inputValue)
-          const data = response.data;
-          setSuggestions(data);
-        } catch (error) {
-          console.error(error);
-        }
-      } else {
-        setSuggestions([]);
+  useEffect(() => {
+    let maybeHandler = (event) => {
+      if (!domNodeRef.current.contains(event.target)) {
+        handler();
       }
     };
-  
-    return (
-      <div>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyUp={autoComplete}
-          placeholder="검색어를 입력하세요..."
-        />
-        <ul>
-            {suggestions.map((suggestion) => (
-            <li key={suggestion.id}>
-                {/* 클릭 시 navigate 함수를 사용하여 페이지 이동 */}
-                <a
-                href={`/movie/${suggestion.tmdbId}`}
-                onClick={(e) => {
-                    e.preventDefault(); // 기본 링크 동작을 막습니다.
-                    navigate(`/movie/${suggestion.tmdbId}`); // 페이지 이동
-                }}
-                >
-                {suggestion.title}
-                </a>
-            </li>
-            ))}
-        </ul>
-      </div>
-    );
-  }
 
+    document.addEventListener('mousedown', maybeHandler);
 
-const MovieGrid = props => {
+    return () => {
+      document.removeEventListener('mousedown', maybeHandler);
+    };
+  });
 
-    const [items, setItems] = useState([]);
-
-    const [page, setPage] = useState(1);
-    const [totalPage, setTotalPage] = useState(0);
-
-    const { keyword } = useParams();
-
-    useEffect(() => {
-        const getList = async () => {
-            let response = null;
-            if (keyword === undefined) {
-                const params = {};
-                switch(props.category) {
-                    case category.movie:
-                        response = await tmdbApi.getMoviesList(movieType.upcoming, {params});
-                        break;
-                    default:
-                        response = await tmdbApi.getTvList(tvType.popular, {params});
-                }
-            } else {
-                const params = {
-                    query: keyword
-                }
-                response = await tmdbApi.search(props.category, {params});
-            }
-            setItems(response.results);
-            setTotalPage(response.total_pages);
-        }
-        getList();
-    }, [props.category, keyword]);
-
-    const loadMore = async () => {
-        let response = null;
-        if (keyword === undefined) {
-            const params = {
-                page: page + 1
-            };
-            switch(props.category) {
-                case category.movie:
-                    response = await tmdbApi.getMoviesList(movieType.upcoming, {params});
-                    break;
-                default:
-                    response = await tmdbApi.getTvList(tvType.popular, {params});
-            }
-        } else {
-            const params = {
-                page: page + 1,
-                query: keyword
-            }
-            response = await tmdbApi.search(props.category, {params});
-        }
-        setItems([...items, ...response.results]);
-        setPage(page + 1);
-    }
-
-    return (
-        <>
-          <div className="section mb-3">
-            <MovieSearch category={props.category} keyword={keyword} />
-          </div>
-          <div className="movie-grid">
-            {items.map((item, i) => (
-              <MovieCard category={props.category} item={item} key={i} />
-            ))}
-          </div>
-          {page < totalPage ? (
-            <div className="movie-grid__loadmore">
-              <OutlineButton className="small" onClick={loadMore}>
-                Load more
-              </OutlineButton>
-            </div>
-          ) : null}
-        </>
-      );
+  return domNodeRef;
 }
 
-const MovieSearch = props => {
+function AutoComplete({ onSearchTermChange }) {
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]); // 제안 목록 상태 추가
+  const navigate = useNavigate();
+  const domNode = useClickOutside(() => {
+    setSuggestions([]); // Clear suggestions when clicking outside
+  });
 
-    const history = useNavigate();
+  // 자동완성 함수 수정
+  const autoComplete = async (searchText) => {
+    if (searchText.length >= 3) {
+      try {
+        const response = await movieSearch(searchText); // API 호출
+        const data = response.data;
+        setSuggestions(data); // 제안 목록 상태 업데이트
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
 
-    const [keyword, setKeyword] = useState(props.keyword ? props.keyword : '');
+  // 입력 값 변경 시 자동완성 함수 호출
+  const onInputChange = (e) => {
+    const newText = e.target.value;
+    setInputValue(newText);
+    autoComplete(newText); // 자동완성 함수 호출
+    if (newText.length === 0) {
+      onSearchTermChange(''); // 입력값이 없으면 검색어 상태도 초기화
+    }
+  };
 
-    const goToSearch = useCallback(
-        () => {
-            if (keyword.trim().length > 0) {
-                history.push(`/${category[props.category]}/search/${keyword}`);
-            }
-        },
-        [keyword, props.category, history]
-    );
+  // 검색 버튼 클릭 시 검색어 상태 업데이트
+  const handleSearch = () => {
+    onSearchTermChange(inputValue);
+  }
 
-    useEffect(() => {
-        const enterEvent = (e) => {
-            e.preventDefault();
-            if (e.keyCode === 13) {
-                goToSearch();
-            }
+  return (
+    <div className="autocomplete-container" ref={domNode}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={onInputChange}
+        placeholder="검색어를 입력하세요..."
+        className="autocomplete-input"
+      />
+      <button onClick={handleSearch} className="autocomplete-button">Search</button>
+      {suggestions.length > 0 && (
+        <ul className="autocomplete-suggestions">
+          {suggestions.map((suggestion) => (
+            <li key={suggestion.id} onClick={() => navigate(`/movie/${suggestion.tmdbId}`)}>
+              {suggestion.title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+const MovieGrid = props => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const updateSearchTerm = (newSearchTerm) => {
+    setSearchTerm(newSearchTerm);
+    setPage(1);
+  }
+
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      const params = { page: page };
+      if (!searchTerm) {
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(movieType.upcoming, { params });
+            break;
+          default:
+            response = await tmdbApi.getTvList(tvType.popular, { params });
         }
-        document.addEventListener('keyup', enterEvent);
-        return () => {
-            document.removeEventListener('keyup', enterEvent);
-        };
-    }, [keyword, goToSearch]);
+      } else {
+        params.query = searchTerm;
+        response = await tmdbApi.search(props.category, { params });
+      }
+      setItems([...(page > 1 ? items : []), ...response.results]);
+      setTotalPage(response.total_pages);
+    }
+    getList();
+  }, [props.category, searchTerm, page, items]);
 
-    return (
-        <div className="movie-search">
-          {/* AutoComplete 컴포넌트를 추가 */}
-          <AutoComplete />
+  const loadMore = async () => {
+    let response = null;
+    const params = { page: page + 1 };
+    if (!searchTerm) {
+      switch (props.category) {
+        case category.movie:
+          response = await tmdbApi.getMoviesList(movieType.upcoming, { params });
+          break;
+        default:
+          response = await tmdbApi.getTvList(tvType.popular, { params });
+      }
+    } else {
+      params.query = searchTerm;
+      response = await tmdbApi.search(props.category, { params });
+    }
+    setItems([...items, ...response.results]);
+    setPage(page + 1);
+  }
 
-          <Button className="small" onClick={goToSearch}>
-            Search
-          </Button>
+  return (
+    <>
+      <div className="movie-search">
+        <AutoComplete onSearchTermChange={updateSearchTerm} />
+      </div>
+      <div className="movie-grid">
+        {items.map((item, i) => (
+          <MovieCard category={props.category} item={item} key={i} />
+        ))}
+      </div>
+      {page < totalPage ? (
+        <div className="movie-grid__loadmore">
+          <OutlineButton className="small" onClick={loadMore}>
+            Load more
+          </OutlineButton>
         </div>
-      );
+      ) : null}
+    </>
+  );
 }
 
 export default MovieGrid;
