5bef190f1550654401bd84ea29dc1f361690ea51
CYPARK <chanyoung990704@naver.com>
2023-12-27 18:24:21 +0900

    change Restful API URI

--- src/api/PostApiService.jsx
+++ src/api/PostApiService.jsx
@@ -1,27 +1,27 @@
 import { appClient } from "../pages/ApiClient";
 
 // 사용자 등록 API 호출
-export const register = (registerForm) => appClient.post('/api/register', registerForm);
+export const register = (registerForm) => appClient.post('/api/members', registerForm);
 
-// 카테고리 별 좋아요 API 호출
-export const likes = (category, id) => appClient.post(`/api/${category}/like/${id}`);
+// 카테고리(영화) 별 좋아요 API 호출
+export const likes = (category, id) => appClient.post(`/api/${category}/${id}/likes`);
 
-// 카테고리 별 좋아요 취소 API 호출
-export const dislikes = (category, id) => appClient.post(`/api/${category}/likecancel/${id}`);
+// 카테고리(영화) 별 좋아요 취소 API 호출
+export const dislikes = (category, id) => appClient.delete(`/api/${category}/${id}/likes`);
 
 // 영화 검색 API 호출
-export const movieSearch = (keyword) => appClient.get(`/autocomplete/search?prefix=${keyword}`);
+export const movieSearch = (keyword) => appClient.get(`/api/movies/search/autocomplete?prefix=${keyword}`);
 
 // 사용자가 좋아한 영화 목록 가져오기
-export const getUserLikedMovies = async () => appClient.get(`/api/member/likemovie`);
+export const getUserLikedMovies = async () => appClient.get(`/api/members/likes/movies`);
 
 // 영화 댓글 가져오기
-export const getMovieComment = (id, page) => appClient.get(`/api/movie/comment/${id}?page=${page}&size=10`);
+export const getMovieComment = (id, page) => appClient.get(`/api/movie/${id}/comments?page=${page}&size=10`);
 
 // 영화에 댓글 달기
 export const postMovieComment = async (movieId, comment) => {
   try {
-    const response = await appClient.post(`/api/movie/comment/${movieId}`, comment, {
+    const response = await appClient.post(`/api/movie/${movieId}/comments`, comment, {
       headers: { 'Content-Type': 'text/plain' }
     });
     return response.data;
@@ -33,7 +33,7 @@ export const postMovieComment = async (movieId, comment) => {
 };
 
 // 영화 추천하기
-export const recommendMovie = (requestData) => appClient.post('/api/movie/recommend', requestData);
+export const recommendMovie = (requestData) => appClient.post('/api/movie/recommendations', requestData);
 
 // 사용자에게 추천된 영화 목록 가져오기
-export const getUserRecommendedMovies = async () => appClient.get(`/api/member/recommendedmovie`);
+export const getUserRecommendedMovies = async () => appClient.get(`/api/members/recommendations/movies`);
